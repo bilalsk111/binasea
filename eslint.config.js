f8e0a7c14864e7de8f17b3b4308f4c215d76b97b
Bilal Shaikh <bilalsk111@gmail.com>
2025-12-23 17:53:53 +0530

    Fix Vite config for Vercel

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,10 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig } from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
+  globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
